18fa0ddb6cf38367ecb4f1db65a072392a8f1416
soumya-2001 <84399483+soumya-2001@users.noreply.github.com>
2021-05-18 21:50:02 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,13 @@
+function purple__btn(){
+    document.body.style.background="purple"
+   }
+   
+   function green__btn()
+   {
+       document.body.style.background="green"
+   }
+   
+   function yellow__btn(){
+       document.body.style.background="yellow"
+   }
+   
